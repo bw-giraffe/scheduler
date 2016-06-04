7e6240fe317dc9226960466990874cb5d56c5d3d
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-06-03 18:52:24 -0700

    setting up sample inputs

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,8 +1,11 @@
 class ScheduleController < ApplicationController
 
   def new
-    p "GOT YOUR PARAMS #{params}" 
-
+    event1 = eval(params[:event1])
+    event2 = eval(params[:event2])
+    event3 = eval(params[:event3])
+    event4 = eval(params[:event4])
+    p "GOT YOUR STUFF #{event1}, #{event2}"
   end
 
 end
