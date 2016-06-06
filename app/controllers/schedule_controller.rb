4c1449309d4fb82c3aa8dedf2d7d52056281f462
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-06-05 20:24:41 -0700

    logic for grouping overlapping events

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -10,6 +10,31 @@ class ScheduleController < ApplicationController
  
     sorted_events = events.sort! { | x, y | x[:end] <=> y[:end] }
     p "YOUR SORTED EVENTS #{sorted_events}"
+    grouped = []
+    grouped.push(sorted_events[0])
+    p "GROUPED HAS FIRST SORTED EVENT #{grouped}"
+    for i in 1..sorted_events.length
+      if(grouped[i-1].class == "Array")
+        if(sorted_events[i][:start] < grouped[i-1].last[:end])
+          grouped[i-1].push(sorted_events[i])
+        else
+          grouped.push(sorted_events[i])
+        end
+      else
+        if(sorted_events[i][:start] < grouped[i-1][:end])
+          blank = []
+          blank.push(grouped[i-1])
+          p "BLANK1 #{blank}"
+          blank.push(sorted_events[i])
+          p "BLANK2 #{blank}"
+          grouped[i-1] = blank
+        else
+          grouped.push(sorted_events[i])
+        end
+      end
+      p "GROUPED SO FAR #{grouped}"
+    end
+
   end
 
 end
