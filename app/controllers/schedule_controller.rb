b9ec1ba83493548051ce6c0a137c699906a12caf
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-06-05 19:10:25 -0700

    sorting input on controller side

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -5,7 +5,11 @@ class ScheduleController < ApplicationController
     event2 = eval(params[:event2])
     event3 = eval(params[:event3])
     event4 = eval(params[:event4])
-    p "GOT YOUR STUFF #{event1}, #{event2}"
+    events = [event1, event2, event3, event4]
+
+ 
+    sorted_events = events.sort! { | x, y | x[:end] <=> y[:end] }
+    p "YOUR SORTED EVENTS #{sorted_events}"
   end
 
 end
